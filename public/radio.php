823b06913efb62ba33f273e8839e9c120224bb2b
KATAOKA Kouta <kd1319008@st.kobedenshi.ac.jp>
2024-05-08 11:07:19 +0900

    ラジオボタン

---
+++ public/radio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>radio.php</title>
+</head>
+
+<body>
+    <h4>0j02004 片岡幸太</h4>
+    <p>ラジオボタン</p>
+    <?php
+    echo '<p>あなたの好きな果物は、' . $_POST[fruit] . 'ですね。</p>';
+    ?>
+    <a href="radio.html">戻る</a>
+</body>
+</html>
